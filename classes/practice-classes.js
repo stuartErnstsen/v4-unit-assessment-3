075353f03cea199494e026edf3c2358ff208a9d3
stuartErnstsen <stuarternstsen@gmail.com>
2020-12-18 09:57:01 -0700

    Completed Classes and Closure sections of Week3 assessment

--- classes/practice-classes.js
+++ classes/practice-classes.js
@@ -15,7 +15,18 @@
 */
 
 //CODE HERE
+class Character {
+  constructor(name, type) {
+    this.name = name,
+      this.type = type
+  }
 
+  getInfo() {
+    return `This is a ${this.type} character named ${this.name}.`
+  }
+}
+
+const char = new Character("Bob", "builder")
 //////////////////PROBLEM 2////////////////////
 
 /*
@@ -33,14 +44,24 @@
 */
 
 //CODE HERE
+class NPC extends Character {
+  constructor(name, type, location, phrase) {
+    super(name, type)
+    this.location = location;
+    this.phrase = phrase;
+  }
 
+  dialogue() {
+    return `${this.name}: ${this.phrase}`
+  }
+}
 /*
-    Create an NPC named Ralph who is a human located in Niceland. His phrase should be `I'm gonna wreck it!`. 
+    Create an NPC named Ralph who is a human located in Niceland. His phrase should be `I'm gonna wreck it!`.
     Store your new NPC in a variable called 'ralph'.
 */
 
 //CODE HERE
-
+const ralph = new NPC('Ralph', 'human', 'Niceland', "I'm gonna wreck it!");
 /*
     Next you're going to create three variables to store information about Ralph.
     First, make a variable named ralphsInfo whose value will be the invocation of Ralph's getInfo method.
@@ -49,24 +70,26 @@
 */
 
 //CODE HERE
-
+const ralphsInfo = ralph.getInfo();
+const ralphsDialogue = ralph.dialogue();
+const ralphsLocation = ralph.location;
 //////////////////PROBLEM 3////////////////////
 
 /*
-  Now you'll make a player class that extends Character 
+  Now you'll make a player class that extends Character
   Each player has all of the same properties as a character with the following additional properties:
     - healthLevel
     - attackLevel
   Each player has the following additional method:
     - defend(amount)
-      - Accepts an amount (another player's attackLevel) as a parameter and subtracts 
+      - Accepts an amount (another player's attackLevel) as a parameter and subtracts
         that amount from the current player's healthLevel
-      - If the healthLevel is above 0, it should return an object with 3 properties. 
-          - The first property should be named 'attackStrength' 
+      - If the healthLevel is above 0, it should return an object with 3 properties.
+          - The first property should be named 'attackStrength'
             and should be equal to the amount that the player is defending against.
-          - The second property should be named 'remainingHealth' 
+          - The second property should be named 'remainingHealth'
             and should be equal to the player's remaining healthLevel.
-          - The third property should be named 'message' 
+          - The third property should be named 'message'
             and should be equal to a string of 'NAME is still in the fight!'
       - Otherwise, it should return a string of 'NAME has been defeated!'
 
@@ -74,26 +97,47 @@
 */
 
 //CODE HERE
+class Player extends Character {
+  constructor(name, type, healthLevel, attackLevel) {
+    super(name, type);
+    this.healthLevel = healthLevel;
+    this.attackLevel = attackLevel;
+  }
 
+  defend(amount) {
+    this.healthLevel -= amount;
+    if (this.healthLevel > 0) {
+      return {
+        attackStrength: amount,
+        remainingHealth: this.healthLevel,
+        message: `${this.name} is still in the fight!`
+      }
+    } else {
+      return `${this.name} has been defeated!`
+    }
+  }
+
+}
 /*
     Next, we'll create two Players.
-    Store the first in a variable called aang, his name should be 'Aang' 
+    Store the first in a variable called aang, his name should be 'Aang'
     and he's an airbender type with a 100 healthLevel and 100 attackLevel.
-    Store the second in a variable called ozai, his name should be 'Ozai' 
+    Store the second in a variable called ozai, his name should be 'Ozai'
     and he's a firebender type with a 100 healthLevel and 0 attackLevel.
 */
 
 //CODE HERE
-
+const aang = new Player('Aang', 'airbender', 100, 100);
+const ozai = new Player('Ozai', 'firebender', 100, 0);
 /*
-    Let's see how a fight between these two would go. 
-    Create a variable called 'battle' whose value is Ozai's 
-    defend method invoked with Aang's attackLevel passed in as an argument. 
+    Let's see how a fight between these two would go.
+    Create a variable called 'battle' whose value is Ozai's
+    defend method invoked with Aang's attackLevel passed in as an argument.
     (You can console log battle to see what happens)
 */
 
 //CODE HERE
-
+const battle = ozai.defend(aang.attackLevel)
 //////////////////PROBLEM 4////////////////////
 
 /*
@@ -111,14 +155,30 @@
 */
 
 //CODE HERE
-
+class Hero extends Player {
+  constructor(name, type, healthLevel, attackLevel) {
+    super(name, type, healthLevel, attackLevel),
+      this.superPowers = []
+  }
+  addSuperPower(power) {
+    this.superPowers.push(power)
+  }
+  useSuperPower(index) {
+    return `${this.name} used ${this.superPowers[index]}!`
+  }
+}
 /*
-  Create a hero named 'Fire Spitter' whose type is 'dragon'. 
-  Fire Spitter's healthLevel and attackLevels should both be 5000. 
+  Create a hero named 'Fire Spitter' whose type is 'dragon'.
+  Fire Spitter's healthLevel and attackLevels should both be 5000.
   Store this information in a variable called fireSpitter.
-  After you create Fire Spitter, add three super powers using the addSuperPower method. 
+  After you create Fire Spitter, add three super powers using the addSuperPower method.
   The first one should be 'spitting fire' and the other two are up to you.
   Last, invoke useSuperPower passing in 0 for the index and store the result in a variable called fireSpitterAttack.
 */
 
 //CODE HERE
+const fireSpitter = new Hero('Fire Spitter', 'dragon', 5000, 5000);
+fireSpitter.addSuperPower('spitting fire')
+fireSpitter.addSuperPower('rapping fire')
+fireSpitter.addSuperPower('beat-boxing')
+const fireSpitterAttack = fireSpitter.useSuperPower(0)
--- closures/practice-closures.js
+++ closures/practice-closures.js
@@ -1,96 +1,127 @@
-  ////////////PROBLEM 1////////////
-  
-  /*
-    Write a function called myFunc. 
-    Inside the function, we'll create a variable and another function. 
-    Call the variable myStr and set it to the string 'super secret string'. 
-    Call the function getSecretString and have it return the myStr variable. 
-    myFunc should return getSecretString
-  */
-  
-  //CODE HERE
+////////////PROBLEM 1////////////
 
-  
-  //Now create a variable called secretString. Its value should be the invocation of myFunc.
+/*
+  Write a function called myFunc. 
+  Inside the function, we'll create a variable and another function. 
+  Call the variable myStr and set it to the string 'super secret string'. 
+  Call the function getSecretString and have it return the myStr variable. 
+  myFunc should return getSecretString
+*/
 
-  //CODE HERE
-  
-  
-  ////////////PROBLEM 2////////////
+//CODE HERE
+function myFunc() {
+  const myStr = 'super secret string';
+  return function getSecretString() {
+    return myStr
+  }
+}
 
-  /*
-    Write a function called lightSwitch. It will return an inner function.
-    Create a variable inside lightSwitch called isTheLightOn and set its initial value to the boolean false. 
-    Write a function called flipTheSwitch inside lightSwitch. 
-    The function should invert the value of isTheLightOn and return a string. 
-    If the light is on (true), the string should be 'The light is on'
-    And if the light is off (false), the string should be 'The light is off'. 
-    The lightSwitch function should return flipTheSwitch.
-  */
-  
-  //CODE HERE
+//Now create a variable called secretString. Its value should be the invocation of myFunc.
 
-  
-  //Create a variable called kitchenSwitch whose value is the invocation of lightSwitch.
-  
-  //CODE HERE
+//CODE HERE
+const secretString = myFunc()
 
-  
-  //Invoke kitchenSwitch.
+////////////PROBLEM 2////////////
 
-  //CODE HERE
-  
-  
-  //Create a variable called bathroomSwitch whose value is the invocation of lightSwitch. 
+/*
+  Write a function called lightSwitch. It will return an inner function.
+  Create a variable inside lightSwitch called isTheLightOn and set its initial value to the boolean false.
+  Write a function called flipTheSwitch inside lightSwitch.
+  The function should invert the value of isTheLightOn and return a string.
+  If the light is on (true), the string should be 'The light is on'
+  And if the light is off (false), the string should be 'The light is off'.
+  The lightSwitch function should return flipTheSwitch.
+*/
 
-  //CODE HERE
-  
-  
-  //Invoke bathroomSwitch twice.
-  
-  //CODE HERE
+//CODE HERE
+function lightSwitch() {
+  let isTheLightOn = false;
+  return flipTheSwitch = () => {
+    if (isTheLightOn) {
+      isTheLightOn = false;
+      return 'The light is off'
+    } else {
+      isTheLightOn = true;
+      return 'The light is on'
+    }
+  }
+}
 
-  
-  
-  ////////////PROBLEM 3////////////
+//Create a variable called kitchenSwitch whose value is the invocation of lightSwitch.
 
-  /*
-    Use the module pattern to create a plant height tracker, name your function 'plantTracker'
-    Set up two variables inside plantTracker, 'plant' which should be set to 'fern' and 'height' which should be set to 12
-    Return 3 functions using the module pattern
-      - 'readInfo' should return a string of 'This is a PLANT plant that is HEIGHT inches tall.', where PLANT is the value of the plant variable and HEIGHT is the value of the height variable.
-      - 'waterPlant' should add 1 to the height and return the new height
-      - 'prunePlant' should subtract 1 from the height and return the new height
-  */
+//CODE HERE
+kitchenSwitch = lightSwitch()
 
-  //CODE HERE
+//Invoke kitchenSwitch.
+
+//CODE HERE
+kitchenSwitch()
+
+//Create a variable called bathroomSwitch whose value is the invocation of lightSwitch. 
+bathroomSwitch = lightSwitch()
+//CODE HERE
 
 
-  ////////////PROBLEM 4////////////
+//Invoke bathroomSwitch twice.
 
-  /*
-    Use the module pattern to create an inventory, name the function 'inventory'
-    Set up a variable inside inventory called 'products' initialized as an empty array.  
-    Return 3 functions using the module pattern
-      - 'readProducts' should return the products array
-      - 'addToProducts' should receive one argument, a string, and add it into the products array
-      - 'deleteFromProducts' should receive one argument, a string, and delete it from the products array
-        - hint: try to find the index of the string first
-  */
+//CODE HERE
+bathroomSwitch()
+bathroomSwitch()
 
-  //CODE HERE
+////////////PROBLEM 3////////////
 
+/*
+  Use the module pattern to create a plant height tracker, name your function 'plantTracker'
+  Set up two variables inside plantTracker, 'plant' which should be set to 'fern' and 'height' which should be set to 12
+  Return 3 functions using the module pattern
+    - 'readInfo' should return a string of 'This is a PLANT plant that is HEIGHT inches tall.', where PLANT is the value of the plant variable and HEIGHT is the value of the height variable.
+    - 'waterPlant' should add 1 to the height and return the new height
+    - 'prunePlant' should subtract 1 from the height and return the new height
+*/
 
-  /*
-    Create a variable called 'shoes' whose value is the invocation of inventory.
-  */
+//CODE HERE
+function plantTracker() {
+  plant = 'fern';
+  height = 12;
+  return {
+    readInfo: () => `This is a ${plant} plant that is ${height} inches tall.`,
+    waterPlant: () => ++height,
+    prunePlant: () => --height
+  }
+}
 
-  //CODE HERE
+////////////PROBLEM 4////////////
 
+/*
+  Use the module pattern to create an inventory, name the function 'inventory'
+  Set up a variable inside inventory called 'products' initialized as an empty array.
+  Return 3 functions using the module pattern
+    - 'readProducts' should return the products array
+    - 'addToProducts' should receive one argument, a string, and add it into the products array
+    - 'deleteFromProducts' should receive one argument, a string, and delete it from the products array
+      - hint: try to find the index of the string first
+*/
 
-  /*
-    Add an item to your shoes array using the addToProducts function
-  */
+//CODE HERE
+function inventory() {
+  const products = [];
+  return {
+    readProducts: () => products,
+    addToProducts: (prod) => products.push(prod),
+    deleteFromProducts: (prod) => products.splice(products.indexOf(prod), 1)
+  }
+}
 
-  //CODE HERE
+/*
+  Create a variable called 'shoes' whose value is the invocation of inventory.
+*/
 
+//CODE HERE
+const shoes = inventory()
+
+/*
+  Add an item to your shoes array using the addToProducts function
+*/
+
+//CODE HERE
+shoes.addToProducts('Dunks')
